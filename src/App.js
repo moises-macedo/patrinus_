0fccd19beca80753573f559046dfcc7f3b3feb54
Eduardo Rangel <eduardo_rp23@hotmail.com>
2022-09-13 19:15:30 -0300

    feature: cria componente de texto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes } from "./Routes";
 import {GLobalStyled} from "./Assets/Styles/global"
+import { TitleScreenIndex } from "./Components/TextScreenIndex";
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <GLobalStyled/>
+    <TitleScreenIndex text="ola"/>
     </>
   );
 }
---
+++ src/Components/TextScreenIndex/index.jsx
@@ -0,0 +1,9 @@
+import { Div } from "./styled"
+
+export const TitleScreenIndex = ({text}) =>{
+    return(
+        <Div>
+            <h2>{text}</h2>
+        </Div>
+    )
+}
