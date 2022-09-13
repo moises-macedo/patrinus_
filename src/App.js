2407a0730eed391938eb40e4472bddbe67b9c767
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-09-13 19:21:59 -0300

    feat: criado Componente para dizer Bem vindo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import { Routes } from "./Routes";
-import {GLobalStyled} from "./Assets/Styles/global"
-
 
 
 function App() {
   return (
-    <>
-    <GLobalStyled/>
-    </>
+    <Routes />
   );
 }
 
