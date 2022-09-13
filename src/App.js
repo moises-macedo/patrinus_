1b7f29946c0c317e00aab65a2fb472d95e190776
Eduardo Rangel <eduardo_rp23@hotmail.com>
2022-09-12 22:01:03 -0300

    cria o card escolas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Routes } from "./Routes";
 import {GLobalStyled} from "./Assets/Styles/global"
-import { InputSearch } from "./Components/InputSearch";
+
 
 
 function App() {
   return (
     <>
     <GLobalStyled/>
-    <InputSearch placeholder="instMix"/>
     </>
   );
 }
---
+++ src/Components/CardShcool/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 250px;
+  height: 250px;
+`;
+export const Content = styled.div`
+  text-align: center;
+
+  p {
+    font-weight: bold;
+    font-size: 1.2rem;
+    margin-top: 20px;
+  }
+
+  figure {
+    width: 250px;
+    height: 250px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
