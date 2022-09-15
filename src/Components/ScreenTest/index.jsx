d44e719dcaeb2984507474981e7479f48cf684b9
JulioBarbosa19 <juliocesarb.silva11@gmail.com>
2022-09-15 20:52:08 -0300

    feat: components AboutMe, InformationUser, SponsoredBy

--- src/Components/ScreenTest/index.jsx
+++ src/Components/ScreenTest/index.jsx
@@ -1,7 +1,16 @@
+import { AboutMe } from "../AboutMe";
 import { Footer } from "../Footer";
+import { InformationUser } from "../InformationUser";
+import { StudentInformation } from "../StudentInformation";
 
 export const ScreenTest = () => {
-  return <Footer/>
+  return (
+  <>
+  <Footer/>
+  <StudentInformation />
+  </>
+    
+  )
 };
 
 
--- src/Components/StudentInformation/style.js
+++ src/Components/StudentInformation/style.js
@@ -3,20 +3,12 @@ import styled from "styled-components";
 export const StudentStyle = styled.div`
    display: flex;
    flex-direction: column;
-   
+   justify-content: center;
    align-items: center;
-   font-family: var(--font-inter);
+  
    font-size: 16.75px;
 
-   h4{
-    margin: 10px;
-   }
-
-   textarea{
-    margin: 10px;
-    background-color: var(--secondary-1);
-    color: var(--dark-1);
-   }
+  
    
    button{
     margin-top: 10px;
