e26c9a664ec41ee1c78a16ea6a50bba574901e60
Eduardo Rangel <eduardo_rp23@hotmail.com>
2022-09-15 22:12:10 -0300

    feature: cria a dashboard cursos favortios e outros componentes como background, card escola e estilização dos mesmo

--- src/Components/ScreenTest/index.jsx
+++ src/Components/ScreenTest/index.jsx
@@ -1,53 +1,5 @@
-import {FavoriteCourses} from "../DashboardStudent/FavoriteCourses"
 import { Footer } from "../Footer";
-import { Menus } from "../Menus";
 
 export const ScreenTest = () => {
-  const pd = [
-    {
-      id: 1,
-      name: "Jaleco",
-      img: " https://th.bing.com/th/id/R.341b0554c8b0e33e8ea9cb88066c506d?rik=cvtoso%2bAflRONA&pid=ImgRaw&r=0"
-    },
-    {
-      id: 2,
-      name: "Avental",
-      img: " https://th.bing.com/th/id/R.341b0554c8b0e33e8ea9cb88066c506d?rik=cvtoso%2bAflRONA&pid=ImgRaw&r=0"
-  
-    },
-    {
-      id: 3,
-      name: "Touca",
-      img:  "https://th.bing.com/th/id/R.341b0554c8b0e33e8ea9cb88066c506d?rik=cvtoso%2bAflRONA&pid=ImgRaw&r=0"
-
-    },
-    {
-      id: 4,
-      name: "Fronha",
-      img:  "https://th.bing.com/th/id/R.341b0554c8b0e33e8ea9cb88066c506d?rik=cvtoso%2bAflRONA&pid=ImgRaw&r=0"
- 
-    },
-    {
-      id: 5,
-      name: "Embalagem",
-      img: "https://th.bing.com/th/id/R.341b0554c8b0e33e8ea9cb88066c506d?rik=cvtoso%2bAflRONA&pid=ImgRaw&r=0"
-
-    },
-    {
-      id: 6,
-      name: "Porta talher",
-      img: " https://th.bing.com/th/id/R.341b0554c8b0e33e8ea9cb88066c506d?rik=cvtoso%2bAflRONA&pid=ImgRaw&r=0"
-
-    },
-    {
-      id: 7,
-      name: "Porta absorvente",
-      img:  "https://th.bing.com/th/id/R.341b0554c8b0e33e8ea9cb88066c506d?rik=cvtoso%2bAflRONA&pid=ImgRaw&r=0"
-    },
-  ];
-  return (
-    <>
-    <FavoriteCourses/>
-    </>
-  );
+  return <Footer />;
 };
