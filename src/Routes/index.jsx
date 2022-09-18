9d14c69ae5254b7586db98db324984dbeba19b82
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-09-18 18:50:16 -0300

    fix: alterado routes

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import { Route, Routes as RoutePages } from "react-router-dom";
 import { ScreenTest } from "../Components/ScreenTest";
+import { Dashboard } from "../Pages/Dashboard";
 import { Home } from "../Pages/Home";
+import { UsersContext } from "../Provider/User";
 
 export const Routes = () => {
+  const { authenticated} =useContext(UsersContext)
   return (
     <RoutePages>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/test" element={<ScreenTest />} />
+      <Route exact path="/" element={<Home authenticated={authenticated} />} />
+      <Route  path="/test" element={<ScreenTest />} />
+      <Route  path="/dashboard" element={<Dashboard authenticated={authenticated}/>} />
     </RoutePages>
   );
 };
