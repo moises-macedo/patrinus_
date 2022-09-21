6ff0e1ba64ad45e03fc01bafd72e545819c20ec9
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-09-21 00:09:01 -0300

    fix: criado componente Courses

--- src/Components/CoursesAvailable/index.jsx
+++ src/Components/CoursesAvailable/index.jsx
@@ -1,24 +1,110 @@
-import { availableItems } from "./item"
+import { availableItems } from "./item";
+import { MdInfo } from "react-icons/md";
+import { useState } from "react";
+import {
+  ContainerDescription,
+  UlDescription,
+  UlTitle,
+  Section,
+  Aside,
+  Title,
+  Description,
+} from "./style";
+import { FiXCircle } from "react-icons/fi";
 
-export const CoursesAvailable = () =>{
-    return(
-        <div>
-            {availableItems.map(({escola, modalidade,image,cursos},index)=>{
-                return(
-                    <ul key={index}>
-                        <li>{escola}</li>
-                        <li>{modalidade}</li>
-                        <li>
-                        {cursos.map(({nome,descricao},index)=>{
-                            return(
-                                <p key={index}>{nome}</p>
-                            )
-                        })}
-                        </li>
+const CoursesDescription = ({ card, index, id = "modal" }) => {
+  const [isClosed, setIsClosed] = useState(false);
 
-                    </ul>
-                )
-            })}
-        </div>
-    )
-}
+  const handleClick = (e) => {
+    if (e.target.id === id) {
+      setIsClosed(!isClosed);
+    }
+    console.log(e.target);
+  };
+  return (
+    <>
+      <UlDescription key={index}>
+        <li>{card.escola}</li>
+        <li>{card.curso}</li>
+        <li>
+          <p>{card.modalidade}</p>
+          <button onClick={() => setIsClosed(!isClosed)}>
+            <p>
+              <MdInfo />
+            </p>
+          </button>
+        </li>
+      </UlDescription>
+
+      {isClosed ? (
+        <Section id={id} onClick={handleClick}>
+          <Aside>
+            <Title>
+              <div>
+                <button onClick={() => setIsClosed(!isClosed)}>
+                  <p>
+                    <FiXCircle />
+                  </p>
+                </button>
+              </div>
+              <h2>Detalhes do curso</h2>
+            </Title>
+            <Description>
+              <ul>
+                <li>
+                  <img src={card.image} alt={card.escola} />
+                </li>
+                <li>
+                  <span>Escola:</span>
+                  <p>{card.escola}</p>
+                </li>
+                <li>
+                  <span>Curso:</span>
+                  <p>{card.curso}</p>
+                </li>
+                <li>
+                  <span>Modalidade:</span>
+                  <p>{card.modalidade}</p>
+                </li>
+                <li>
+                  <span>Descrição:</span>
+                  <p>{card.descricao}</p>
+                </li>
+                <li>
+                  <button>Inscrever-se</button>
+                </li>
+              </ul>
+            </Description>
+          </Aside>
+        </Section>
+      ) : null}
+    </>
+  );
+};
+
+const CoursesTitle = () => {
+  return (
+    <UlTitle>
+      <li>
+        <p>Escola</p>
+      </li>
+      <li>
+        <p>Curso</p>
+      </li>
+      <li>
+        <p>Modalidade</p>
+      </li>
+    </UlTitle>
+  );
+};
+
+export const CoursesAvailable = () => {
+  return (
+    <ContainerDescription>
+      <CoursesTitle />
+      {availableItems.map((el, index) => {
+        return <CoursesDescription card={el} key={index} />;
+      })}
+    </ContainerDescription>
+  );
+};
---
+++ src/Components/CoursesAvailable/style.js
@@ -0,0 +1,226 @@
+import styled from "styled-components";
+
+export const UlTitle = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background: var(--primary-2);
+  border: 1px solid #e1e1e1;
+
+  li {
+    min-height: 50px;
+    font-family: var(--font-barlow);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    p {
+      color: var(--dark-2);
+      font-size: 20px;
+      font-weight: 600;
+    }
+    &:nth-child(1) {
+      width: 43%;
+    }
+    &:nth-child(2) {
+      width: 23%;
+      justify-content: flex-start;
+    }
+    &:nth-child(3) {
+      width: 33%;
+    }
+  }
+`;
+export const ContainerDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const UlDescription = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  border: 1px solid #e1e1e1;
+  li {
+    min-height: 50px;
+    font-family: var(--font-barlow);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:nth-child(1) {
+      width: 43%;
+    }
+    &:nth-child(2) {
+      width: 23%;
+      justify-content: flex-start;
+    }
+    &:nth-child(3) {
+      width: 33%;
+    }
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      margin-left: 10px;
+      background: none;
+      color: var(--primary-2);
+
+      &:hover {
+        color: var(--primary-1);
+      }
+    }
+  }
+`;
+
+export const Section = styled.section`
+  min-height: 100vh;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 10;
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow-y: none;
+  background: rgba(0, 0, 0, 0.4);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+`;
+export const Aside = styled.aside`
+  width: 100%;
+  max-width: 500px;
+  background: var(--light-2);
+  min-height: 100vh;
+`;
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 80px;
+  margin-bottom: 20px;
+  div {
+    width: 95%;
+    display: flex;
+    justify-content: flex-end;
+    button {
+      background: none;
+      cursor: pointer;
+      p {
+        font-size: 20px;
+        color: var(--dark-2);
+
+        &:hover {
+          color: var(--dark-1);
+        }
+      }
+    }
+  }
+`;
+
+export const Description = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ul {
+    width: 95%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    li {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      min-height: 50px;
+      border-bottom: 1px solid #e1e1e1;
+      img {
+        width: 100%;
+        max-width: 150px;
+        margin-bottom: 5px;
+      }
+      span {
+        font-weight: bold;
+      }
+      p {
+        margin-left: 10px;
+        font-size: 18px;
+      }
+      &:nth-child(5) {
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        p {
+          margin-left: 0;
+          margin-top: 10px;
+          text-align: justify;
+        }
+      }
+      &:nth-child(6) {
+        margin-top: 30px;
+        border: none;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 10px;
+        button {
+            width: 100%;
+          font-size: 18px;
+          letter-spacing: 2px;
+          text-transform: uppercase;
+          display: inline-block;
+          text-align: center;
+          font-weight: bold;
+          padding: 1em 2em;
+          border: 3px solid var(--primary-1);
+          border-radius: 2px;
+          position: relative;
+          box-shadow: 0 2px 10px rgba(0, 0, 0, 0.16),
+            0 3px 6px rgba(0, 0, 0, 0.1);
+          color: var(--primary-1);
+          text-decoration: none;
+          transition: 0.3s ease all;
+          z-index: 1;
+          cursor: pointer;
+
+          &::before {
+            transition: 0.5s all ease;
+            position: absolute;
+            top: 0;
+            left: 50%;
+            right: 50%;
+            bottom: 0;
+            opacity: 0;
+            content: "";
+            background-color: var(--primary-1);
+            z-index: -1;
+          }
+          &:hover,
+          &:focus {
+            color: var(--light-2);
+          }
+          &:hover:before,
+          &:focus:before {
+            transition: 0.5s all ease;
+            left: 0;
+            right: 0;
+            opacity: 1;
+          }
+          &:active {
+            transform: scale(0.9);
+          }
+        }
+
+
+
+      }
+    }
+  }
+`;
