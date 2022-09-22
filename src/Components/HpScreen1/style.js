4248efe90a44a39cfb319618123c94da75aa66b3
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-09-22 20:28:17 -0300

    Update style.js

--- src/Components/HpScreen1/style.js
+++ src/Components/HpScreen1/style.js
@@ -58,8 +58,8 @@ export const Content = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: pink;
-  /* cor certa: #83ad93 */
+  background-color: #83ad93;
+
 
   header {
     margin-top: 10px;
